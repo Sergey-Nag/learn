22d8d9cfd439f58460fb0326a0ab87dbc4e0f876
Serhii Nahornyi <nahornyi@hpe.com>
2024-02-11 20:39:34 +0200

    asdasdf

---
+++ project/scripts/index.js
@@ -0,0 +1,13 @@
+const el = document.getElementById('block');
+const btn = document.getElementById('btn');
+
+let angle = 0;
+// setInterval(() => {
+//     el.style.transform = `rotate(${angle}deg)`;
+//     angle += 10;
+// }, 200);
+hello = 1
+btn.addEventListener('click', () => {
+    confirm('Are you sure?');
+});
+
